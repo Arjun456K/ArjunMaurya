e6835fb75894858e5997d3a086add21f7fd39cc8
Arjun456K <55030696+Arjun456K@users.noreply.github.com>
2019-09-08 08:01:13 -0700

    Add files via upload

---
+++ ComparisionMain.java
@@ -0,0 +1,60 @@
+
+  
+package com.ritroorkee.level2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.ritroorkee.Secretary;
+
+public class ComparisionMain {
+
+	public static void main(String[] args) {
+		List<Secretary> ss = new ArrayList<Secretary>();
+
+		Secretary aman = new Secretary(22, "XAman", "HR");
+		ss.add(aman);
+
+		Secretary sx = new Secretary(1, "Sikha", "HR");
+		ss.add(sx);
+
+		Secretary maurya;
+		try {
+			// https://docs.oracle.com/javase/7/docs/api/java/lang/Throwable.html
+			maurya = new Secretary(3, "Maurya G", null);
+			ss.add(maurya);
+		} catch (Exception e) {
+			
+		}
+		maurya = new Secretary(4, "Luv G", "MCA");
+		ss.add(maurya);
+	
+	
+
+
+
+		maurya = new Secretary(5, "Pooja G", "MECH");
+		ss.add(maurya);
+	
+
+
+		maurya = new Secretary(10, "Sandeep G", "Mech");
+		ss.add(maurya);
+
+		maurya= new Secretary(7, "Deepa G", "CSE");
+		ss.add(maurya);
+		maurya=new Secretary(8,"Santosh","MCA");
+		ss.add(maurya);
+		
+
+		ss.forEach(s -> System.out.println(s));
+		System.out.println("----------------");
+		Collections.sort(ss, 
+				Comparator.comparing(Secretary::getDept)
+				.thenComparing(Secretary::getName));
+		ss.forEach(s -> System.out.println(s));
+
+	}
+}
